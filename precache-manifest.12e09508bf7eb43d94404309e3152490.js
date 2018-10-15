fe3eb2a8d790a019757d86602280585d8153a68f
Balasubramani M <bala.m@live.in>
2018-10-15 12:03:32 +0530

    Updates

---
+++ precache-manifest.12e09508bf7eb43d94404309e3152490.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meesho-frontend-challenge/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2abd4de66e93840ff4d0",
+    "url": "/meesho-frontend-challenge/static/js/runtime~main.2abd4de6.js"
+  },
+  {
+    "revision": "4094ff4013a440a5f030",
+    "url": "/meesho-frontend-challenge/static/js/main.4094ff40.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/meesho-frontend-challenge/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "4094ff4013a440a5f030",
+    "url": "/meesho-frontend-challenge/static/css/main.f23843b6.chunk.css"
+  },
+  {
+    "revision": "707ee1d1371bce46328f1685a04012be",
+    "url": "/meesho-frontend-challenge/index.html"
+  }
+];
